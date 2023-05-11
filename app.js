ca5bcaf4fd578ec4e16253809c5ef9e7e1466cca
yemiliZ <vaniazaragoza@gmail.com>
2023-05-11 15:00:39 +0000

    recarga

---
+++ app.js
@@ -0,0 +1,41 @@
+// Importar modulo http y exprexx
+import http from "http";
+import express from "express";
+
+// Crear una instancia de express
+const app = express(); // (req, res) => {UN MONTON DE CODIGO}
+
+// Registrar nuestro primer middleware
+app.use((req, res, next)=> {
+    console.log("Ejecutando el Middleware 1");
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log("Ejecutando el Misddleware 2");
+    next()
+});
+app.use((req, res, next)=>{
+    console.log(`${req.method} - ${req.url}`);
+    next();
+});
+app.use((req, res, next)=>{
+    console.log("Respondiendo al cliente");
+    res.send(`
+    <h1>Welcome to express</h1>
+    <p>This is my awesome app</p>
+    `);
+});
+// Creando el servidor
+const server = http.createServer(app);
+
+// Definir puertos
+const port = 3000;
+const ip = "0.0.0.0";
+
+// Arrancar el servidor
+server.listen(port, ip, (err) => {
+
+    console.log("Funcionando en http://localhost:3000");
+
+});
